c6a3d4ed3778649168e81ea861de13fd0b5b3fc7
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2024-05-20 01:25:31 +0700

    Fixed issues with Lombok annotations in Book model class
    Enabled Lombok annotation processing and fixed the 'Data' annotation issue in the Book class by removing redundant getter and setter methods. Also made minor updates in the build.gradle and modules.xml files for proper compilation and execution.

--- src/main/java/org/example/springjpatutorial/model/Book.java
+++ src/main/java/org/example/springjpatutorial/model/Book.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
-@Data //TODO doesn't work for some reason, fix that latter
+@Data
 public class Book {
     @Id
     @Hidden
@@ -19,54 +19,5 @@ public class Book {
     private String title;
     private String author;
     private String publisher;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     private String status;
 }
