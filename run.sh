a671fb4c2c1a0bb5ffca6930ec4a4cc4a5a112a5
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2024-05-24 17:28:27 +0700

    Use parameter in run.sh, so it's easier to read Specify hibernate.dialect

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-REDIS_DOCKER_NAME="redis"
 DATABASE_DOCKER_NAME="mysql"
+DATABASE_DOCKER_USER="sorravit"
+DATABASE_DOCKER_PASSWORD="sorravit"
+DATABASE_DOCKER_DATABASE_NAME="big"
+DATABASE_DOCKER_ROOT_PASSWORD="sorravit"
 
 goal_database_start() {
-  docker run --detach --env MYSQL_ROOT_PASSWORD=sorravit --env MYSQL_USER=sorravit --env MYSQL_PASSWORD=sorravit --env MYSQL_DATABASE=big --name ${DATABASE_DOCKER_NAME} --publish 3306:3306 mysql
+  docker run --detach --env MYSQL_ROOT_PASSWORD=${DATABASE_DOCKER_ROOT_PASSWORD} --env MYSQL_USER=${DATABASE_DOCKER_USER} --env MYSQL_PASSWORD=${DATABASE_DOCKER_PASSWORD} --env MYSQL_DATABASE=${DATABASE_DOCKER_DATABASE_NAME} --name ${DATABASE_DOCKER_NAME} --publish 3306:3306 mysql
 }
 goal_database_stop() {
   docker stop ${DATABASE_DOCKER_NAME}
@@ -26,6 +29,7 @@ else
     goal:
         database_start                      -- start database from mysql docker image
         database_stop                       -- stop database from mysql docker image
+        database_clear                      -- stop and remove database from mysql docker image
         redis_start                         -- start redis from redis docker image
         redis_stop                          -- stop redis docker
         "
